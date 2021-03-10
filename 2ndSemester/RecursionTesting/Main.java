227ea4ec966237f4ba4d0746e702790da709db5b
alpasaam <alpasaam422gmailcom>
2021-03-09 17:11:20 -0700

    f

--- 2ndSemester/RecursionTesting/Main.java
+++ 2ndSemester/RecursionTesting/Main.java
@@ -1,3 +1,41 @@
-Hello World
-Second Attempt
-Third on atom 
+public class Main {
+  public int n;
+
+  // recursive
+  public void factorial(double n) {
+    if (n == 1) {
+      return n;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+
+  // Traditional
+  public void tFactorial(double n) {
+    for(int i = n; i <= 100; i++) {
+      tFactorial = tFactorial * i;
+    }
+    return tFactorial;
+  }
+
+  // Traditional Fibonacci
+  public void tFibonacci(double n) {
+    if (n == 1 || n == 0) {
+      return n;
+    }
+    return tFibonacci(n - 1) + tFibonacci(n - 2);
+  }
+
+  // Recursive Fibonacci
+  public void rFibonacci(double n) {
+    if (n == 0) {
+      return 0;
+    }
+    else if (n == 1) {
+      return 1;
+    }
+    else {
+      return rFibonacci(n - 1) + rFibonacci(n - 2);
+    }
+  }
+}

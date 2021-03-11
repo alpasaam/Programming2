830d3d9bcad9e33878cd4a02fe1407b7a8916260
alpasaam <alpasaam422@gmail.com>
2021-03-11 13:37:07 -0700

    final code

--- 2ndSemester/PermutationWithRecursion/Main.java
+++ 2ndSemester/PermutationWithRecursion/Main.java
@@ -2,6 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
     PermutationIterator s = new PermutationIterator();
-    System.out.println(s.printPermutn("abc",""));
+    System.out.println(s.permutations("abc"));
   }
 }
--- 2ndSemester/PermutationWithRecursion/PermutationIterator.java
+++ 2ndSemester/PermutationWithRecursion/PermutationIterator.java
@@ -1,4 +1,4 @@
-// import java.util.ArrayList;
+import java.util.ArrayList;
 /**
    This class generates permutations of a word.
 */
@@ -11,24 +11,42 @@ public class PermutationIterator {
       Permutation iterator for given word.
       @param aWord the word to permute
    */
+   public static ArrayList<String> permutations(String word) {
+      ArrayList<String> result = new ArrayList<String>();
+
+      // The empty string has a single permutation: itself
+      if (word.length() == 0) {
+         result.add(word);
+         return result;
+      } else {
+        for (int i = 0; i < word.length(); i++) {
+          String shorter = word.substring(0, i) + word.substring(i + 1);
+          ArrayList<String> shorterPermutations = permutations(shorter);
+          for (String s : shorterPermutations) {
+            result.add(word.charAt(i) + s);
+          }
+        }
+        return result;
+      }
+    }
    public PermutationIterator() {
 
    }
-
-   public String printPermutn(String str, String ans) {
-     //If String is empty
-     if (str.length() == 0) {
-       System.out.print(ans + " ");
-       return ans + "";
-     }
-     for (int i = 0; i<str.length(); i ++) {
-       char ch = str.charAt(i);
-
-       String ros = str.substring(0,i) + str.substring(i+1);
-
-       printPermutn(ros, ans + ch);
-     }
-   }
+   //
+   // public String printPermutn(String str, String ans) {
+   //   //If String is empty
+   //   if (str.length() == 0) {
+   //     System.out.print(ans + " ");
+   //     return ans + "";
+   //   }
+   //   for (int i = 0; i<str.length(); i ++) {
+   //     char ch = str.charAt(i);
+   //
+   //     String ros = str.substring(0,i) + str.substring(i+1);
+   //
+   //     printPermutn(ros, ans + ch);
+   //   }
+   // }
 //
 //
 //    /**

bb06bad9746ab3827154c37d7620d6e9eb50dabf
alpasaam <alpasaam422@gmail.com>
2021-03-11 12:03:46 -0700

    hj

--- 2ndSemester/PermutationWithRecursion/Main.java
+++ 2ndSemester/PermutationWithRecursion/Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
+    PermutationIterator s = new PermutationIterator();
+    System.out.println(s.printPermutn("abc",""));
   }
 }
--- 2ndSemester/PermutationWithRecursion/PermutationIterator.java
+++ 2ndSemester/PermutationWithRecursion/PermutationIterator.java
@@ -1,67 +1,82 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 /**
    This class generates permutations of a word.
 */
 public class PermutationIterator {
-   private String word;
-   private int current;
-   private PermutationIterator tailIterator;
-   private String tail;
+   // private String word;
+   // private int current;
+   // private PermutationIterator tailIterator;
+   // private String tail;
    /**
       Permutation iterator for given word.
       @param aWord the word to permute
    */
-   public PermutationIterator(String aWord)
-   {
-      // How would you implement this method recursively?
+   public PermutationIterator() {
+
    }
 
-   /**
-      Generate next permutation.
-      @return next permutation
-   */
-   public String nextPermutation()
-   {
-      if (word.length() == 0)
-      {
-         current++;
-         return "";
-      }
+   public String printPermutn(String str, String ans) {
+     //If String is empty
+     if (str.length() == 0) {
+       System.out.print(ans + " ");
+       return ans + "";
+     }
+     for (int i = 0; i<str.length(); i ++) {
+       char ch = str.charAt(i);
 
-      char c = word.charAt(current);
-      String nextPermut = tailIterator.nextPermutation();
+       String ros = str.substring(0,i) + str.substring(i+1);
 
-      if (!tailIterator.hasMorePermutations())
-      {
-         current++;
-         if (current >= word.length())
-         {
-            tailIterator = null;
-         }
-         else
-         {
-            if (current + 1 >= word.length())
-            {
-               tail = word.substring(0, current);
-            }
-            else
-            {
-               tail = word.substring(0, current)
-                     + word.substring(current + 1, word.length());
-            }
-            tailIterator = new PermutationIterator(tail);
-         }
-      }
-      return c + nextPermut;
+       printPermutn(ros, ans + ch);
+     }
    }
-
-   /**
-      Determine if there are more permutations.
-      @return true if more permutations
-   */
-   public boolean hasMorePermutations()
-   {
-      return current < word.length() || tailIterator != null
-            && tailIterator.hasMorePermutations();
-   }
+//
+//
+//    /**
+//       Generate next permutation.
+//       @return next permutation
+//    */
+//    public String nextPermutation()
+//    {
+//       if (word.length() == 0)
+//       {
+//          current++;
+//          return "";
+//       }
+//
+//       char c = word.charAt(current);
+//       String nextPermut = tailIterator.nextPermutation();
+//
+//       if (!tailIterator.hasMorePermutations())
+//       {
+//          current++;
+//          if (current >= word.length())
+//          {
+//             tailIterator = null;
+//          }
+//          else
+//          {
+//             if (current + 1 >= word.length())
+//             {
+//                tail = word.substring(0, current);
+//             }
+//             else
+//             {
+//                tail = word.substring(0, current)
+//                      + word.substring(current + 1, word.length());
+//             }
+//             tailIterator = new PermutationIterator(tail);
+//          }
+//       }
+//       return c + nextPermut;
+//    }
+//
+//    /**
+//       Determine if there are more permutations.
+//       @return true if more permutations
+//    */
+//    public boolean hasMorePermutations()
+//    {
+//       return current < word.length() || tailIterator != null
+//             && tailIterator.hasMorePermutations();
+//    }
 }
